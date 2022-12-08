8e9b6662f8bacdeafff51109127404ba3b3c1b19
Brian Whiting <brian@redpeppersoftware.com>
2022-12-08 14:27:52 -0700

    Updated site to use Destiny Collections name

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,10 @@
 import React, { Fragment } from 'react';
 
-import { saveDataExplorerVisited } from 'app/lib/ls';
-
 import styles from './styles.styl';
-
-import DonateButton from 'app/components/DonateButton';
-
-const rememberDev = () => saveDataExplorerVisited(true);
 
 export default function Footer({ item, children }) {
   return (
     <div className={styles.footer}>
-      <DonateButton />
       {children && (
         <Fragment>
           <br />
@@ -19,20 +12,16 @@ export default function Footer({ item, children }) {
           <br />
         </Fragment>
       )}
-      <br />
-      Developer, or curious about the data behind Destiny? Check out the{' '}
+      Maintained by{' '}
       <a
-        href="https://data.destinysets.com"
+        href="https://github.com/BrianWhiting"
         target="_blank"
         rel="noopener noreferrer"
-        onClick={rememberDev}
       >
-        Destiny Data Explorer
-      </a>
-      .
+        Brian Whiting
+      </a>.
       <br />
-      <br />
-      Made by{' '}
+      Originally made by{' '}
       <a
         href="https://twitter.com/joshhunt"
         target="_blank"
@@ -52,7 +41,7 @@ export default function Footer({ item, children }) {
       <br />
       Having issues or need help?{' '}
       <a
-        href="https://github.com/joshhunt/destinySets/issues"
+        href="https://github.com/BrianWhiting/DestinyCollections-Legacy/issues"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -79,10 +68,7 @@ export default function Footer({ item, children }) {
       for the Eververse Bright Dust schedule.
       <br />
       <br />
-      Destiny is a registered trademark of Bungie. Data and images sourced from
-      Bungie.
-      <br />
-      Copyright © World Class Development Ltd 2018
+      Destiny is a registered trademark of Bungie. Data and images sourced from Bungie.
     </div>
   );
 }
